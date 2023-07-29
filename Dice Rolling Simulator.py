5fa9e1bf46111e8f7350bd2045b0785e98814c35
Nadia Fallahtafti <33514608+nadiaft72@users.noreply.github.com>
2023-07-29 23:38:30 +0330

    Update Dice Rolling Simulator.py
    add comments

--- Dice Rolling Simulator.py
+++ Dice Rolling Simulator.py
@@ -1,7 +1,19 @@
+# Import the random module to generate random numbers.
 import random
+
+# Ask the user if they want to roll a dice or not.
 end = input("to roll a dice enter y if not enter n: ")
+
+# Start a loop to keep rolling the dice as long as the user enters 'y'.
 while end == "y":
+    # Generate a random number between 1 and 6 to simulate a dice roll.
     x = random.randint(1,6)
+    
+    # Print the result of the dice roll to the user.
     print("your dice number is : ",x," \n")
+    
+    # Ask the user again if they want to roll the dice or not.
     end = input("to roll a dice enter y if not enter n: ")
+
+# The loop ends when the user enters 'n'. Print a fun message to conclude the program.
 print("\n have fun!")
