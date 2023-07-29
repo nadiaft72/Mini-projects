bc9e0b8d55b08230e57fd386c97d4f250be7c6e2
Nadia Fallahtafti <33514608+nadiaft72@users.noreply.github.com>
2023-07-29 23:45:48 +0330

    Update Number Guessing.py
    add comment

--- Number Guessing.py
+++ Number Guessing.py
@@ -1,14 +1,26 @@
+# Import the random module to generate a random number.
 import random
 
-x = random.randint(1,100)
-print(x)
-Gnum = int(input("guess a number (1-100): "))
-while Gnum != x :
-    if Gnum > x :
-        print("ur guess is higher than mine ! :)")
-    elif Gnum < x :
-        print("ur guess is lower than mine ! :)")
-    Gnum = int(input("guess a number (1-100): "))
+# Generate a random number between 1 and 100 (inclusive) and store it in 'x'.
+x = random.randint(1, 100)
 
+# For debugging purposes, print the randomly generated number.
+print("Random number:", x)
 
-print ("yay you did it! :) ")
+# Ask the user to guess a number between 1 and 100 and convert the input to an integer.
+Gnum = int(input("Guess a number (1-100): "))
+
+# Start a loop to keep prompting the user for guesses until the correct number is guessed.
+while Gnum != x:
+    # If the guessed number is greater than the random number.
+    if Gnum > x:
+        print("Your guess is higher than mine! :)")
+    # If the guessed number is less than the random number.
+    elif Gnum < x:
+        print("Your guess is lower than mine! :)")
+
+    # Ask the user to guess again and update 'Gnum' with the new guess.
+    Gnum = int(input("Guess a number (1-100): "))
+
+# The loop ends when the user guesses the correct number.
+print("Yay, you did it! :) ")
